15e41cd5574f34550b3b4a255389d38661ace53a
Pedro Nunes Pereira <pedro.nunes-pereira@siemens.com>
2025-09-16 13:12:54 +0100

    minor refactor

--- Main.qml
+++ Main.qml
@@ -17,24 +17,5 @@ Window {
                 width: parent.width
                 height: parent.height
             }
-
-            Rectangle {
-                color: Qt.rgba(0.3, 0.7, 0.5, 0.7)
-                border.width: 1
-                border.color: "#339999"
-                anchors.centerIn: parent
-                width : parent.width - 100
-                height: 50
-                radius: 8
-
-                Text{
-                    text: "Demo for Gstreamer"
-                    anchors.fill: parent
-                    verticalAlignment: Qt.AlignVCenter
-                    horizontalAlignment: Qt.AlignHCenter
-                    font.pointSize: 24
-                }
-
-            }
         }
 }
--- main.cpp
+++ main.cpp
@@ -2,85 +2,76 @@
 #include <QQmlApplicationEngine>
 #include <gst/gst.h>
 
-#include <QQuickWindow>
 #include <QQuickItem>
+#include <QQuickWindow>
 #include <QRunnable>
 
-class SetPlaying : public QRunnable
-{
+class SetPlaying : public QRunnable {
 public:
-    SetPlaying(GstElement *);
-    ~SetPlaying();
+  SetPlaying(GstElement *);
+  ~SetPlaying();
 
-    void run ();
+  void run();
 
 private:
-    GstElement * pipeline_;
+  GstElement *pipeline_;
 };
 
-SetPlaying::SetPlaying (GstElement * pipeline)
-{
-    this->pipeline_ = pipeline ? static_cast<GstElement *> (gst_object_ref (pipeline)) : NULL;
+SetPlaying::SetPlaying(GstElement *pipeline) {
+  this->pipeline_ =
+      pipeline ? static_cast<GstElement *>(gst_object_ref(pipeline)) : NULL;
 }
 
-SetPlaying::~SetPlaying ()
-{
-    if (this->pipeline_)
-        gst_object_unref (this->pipeline_);
+SetPlaying::~SetPlaying() {
+  if (this->pipeline_)
+    gst_object_unref(this->pipeline_);
 }
 
-void
-SetPlaying::run ()
-{
-    if (this->pipeline_)
-        gst_element_set_state (this->pipeline_, GST_STATE_PLAYING);
+void SetPlaying::run() {
+  if (this->pipeline_)
+    gst_element_set_state(this->pipeline_, GST_STATE_PLAYING);
 }
 
 G_BEGIN_DECLS
-    GST_PLUGIN_STATIC_DECLARE(qml6);
+GST_PLUGIN_STATIC_DECLARE(qml6);
 
 G_END_DECLS
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
-    gst_init(&argc, &argv);
-    GST_PLUGIN_STATIC_REGISTER(qml6);
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+  QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+  gst_init(&argc, &argv);
+  GST_PLUGIN_STATIC_REGISTER(qml6);
 
+  /* the plugin must be loaded before loading the qml file to register the
+   * GstGLVideoItem qml item */
 
-    /* the plugin must be loaded before loading the qml file to register the
-     * GstGLVideoItem qml item */
+  QQmlApplicationEngine engine;
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+      []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+  engine.loadFromModule("untitled10", "Main");
+  QQuickItem *videoItem;
+  QQuickWindow *rootObject;
 
+  /* find and set the videoItem on the sink */
+  rootObject = static_cast<QQuickWindow *>(engine.rootObjects().first());
+  videoItem = rootObject->findChild<QQuickItem *>("videoItem");
+  g_assert(videoItem);
+  GstElement *sink = gst_element_factory_make("qml6glsink", NULL);
 
+  GstElement *_testpipeline = gst_parse_launch(
+      "souphttpsrc location=http://127.0.0.1:5000/stream ! multipartdemux ! jpegdec ! videoconvert !\
+          glupload ! glcolorconvert ! qml6glsink name=sink",
+      NULL);
+  GstElement *testsink = gst_bin_get_by_name((GstBin *)_testpipeline, "sink");
 
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("untitled10", "Main");
-    QQuickItem *videoItem;
-    QQuickWindow *rootObject;
+  g_assert(testsink);
+  g_object_set(testsink, "widget", videoItem, NULL);
 
-    /* find and set the videoItem on the sink */
-    rootObject = static_cast<QQuickWindow *> (engine.rootObjects().first());
-    videoItem = rootObject->findChild<QQuickItem *> ("videoItem");
-    g_assert (videoItem);
-    GstElement *sink = gst_element_factory_make ("qml6glsink", NULL);
-
-    GstElement *_testpipeline = gst_parse_launch("videotestsrc ! videoconvert  !\
-          glupload ! glcolorconvert ! qml6glsink name=sink",NULL);
-    GstElement * testsink = gst_bin_get_by_name((GstBin*)_testpipeline,"sink");
-
-    g_assert (testsink);
-    g_object_set(testsink, "widget", videoItem, NULL);
-
-    rootObject->scheduleRenderJob (new SetPlaying (_testpipeline),
-                                  QQuickWindow::BeforeSynchronizingStage);
-    return app.exec();
-    gst_element_set_state (_testpipeline, GST_STATE_NULL);
-    gst_object_unref (_testpipeline);
-    gst_deinit ();
+  rootObject->scheduleRenderJob(new SetPlaying(_testpipeline),
+                                QQuickWindow::BeforeSynchronizingStage);
+  return app.exec();
+  gst_element_set_state(_testpipeline, GST_STATE_NULL);
+  gst_object_unref(_testpipeline);
+  gst_deinit();
 }
